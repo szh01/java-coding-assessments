bab7fcfdbf26a2f914b2260aea0b80670d89bb95
szh01 <sumzh95@gmail.com>
2022-05-08 20:55:38 +0800

    update question 7-1, 7-2

--- src/assessments/assessment7/QuestionOne.java
+++ src/assessments/assessment7/QuestionOne.java
@@ -1,12 +1,26 @@
 package assessments.assessment7;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class QuestionOne {
-    public static void main(String[] args) {
-        System.out.println("Input 1");
-        Scanner in = new Scanner(System.in);
-        String str1 = in.nextLine();
-        String str2 = in.nextLine();
+    public static void main(String[] args) throws IOException {
+        Path filePath = Path.of("File1.txt");
+        StringBuilder contentBuilder = new StringBuilder();
+        try (Stream<String> stream= Files.lines(Paths.get(String.valueOf(filePath)), StandardCharsets.UTF_8))
+        {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        String fileContent = contentBuilder.toString();
+        System.out.println(fileContent);
     }
+
 }
--- src/assessments/assessment7/QuestionTwo.java
+++ src/assessments/assessment7/QuestionTwo.java
@@ -1,5 +1,6 @@
 package assessments.assessment7;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class QuestionTwo {
@@ -7,6 +8,13 @@ public class QuestionTwo {
         System.out.println("Input 1");
         Scanner in = new Scanner(System.in);
         String str1 = in.nextLine();
-
+        int counter = Integer.parseInt(str1);
+        String[] array = new String[counter];
+        for(int i=0; i<counter; i++){
+            String str = in.nextLine();
+            array[i] = str;
+        }
+        Arrays.sort(array);
+        for (String str : array) System.out.println(str);
     }
 }
